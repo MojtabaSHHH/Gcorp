b1b5137c7f47cf9f5151dfcb9fbccec76f4b94b4
Mojtaba Shabanzadeh <mojtabashhh@gmail.com>
2024-05-22 17:10:42 +0330

    added user and admin and login and logout and reset pass

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,17 @@
 import express from 'express';
+import authRouter from "./auth/auth.routes";
+import adminRoutes from "./admin/admin.routes";
+import userRoutes from "./user/user.routes";
+import UserAuthMiddleware from './middleware/UserAuthMiddleware';
 
 const router = express.Router();
+const getUser = require("./middleware/getUserFromCookie");
+const checkTokens = require("./middleware/checkTokens");
 
+router.use("/auth", authRouter);
+
+router.use("/admin", UserAuthMiddleware, adminRoutes);
+
+router.use("/user", getUser(), checkTokens(), UserAuthMiddleware, userRoutes);
 
 export default router;
